b3e9ed9e82700517035a2b81b8ef543de430dd23
ghassen <ghassendhaoui.eng@gmail.com>
2026-02-08 21:44:21 +0100

    user module entities

---
+++ src/main/java/tn/finix/finixbackend/modules/user/model/StatusType.java
@@ -0,0 +1,8 @@
+package tn.finix.finixbackend.modules.user.model;
+
+public enum StatusType {
+    SUSPENDED,
+    ACTIVE,
+    INACTIVE,
+    PENDING,
+}
--- src/main/java/tn/finix/finixbackend/modules/user/model/User.java
+++ src/main/java/tn/finix/finixbackend/modules/user/model/User.java
@@ -1,5 +1,28 @@
 package tn.finix.finixbackend.modules.user.model;
 
-//base user class
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+
+    private long id;
+    private String name;
+    private String lastName;
+    private String email;
+    private String password;
+    private String telephone;
+    private String address;
+    private RoleType role;
+    private StatusType status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private Integer profileCompletion = 0; //0..100
+
+
 }
