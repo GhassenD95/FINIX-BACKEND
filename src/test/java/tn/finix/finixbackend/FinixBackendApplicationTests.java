76c488663df917c97668e5d72f72842057e1b1bd
ghassen <ghassendhaoui.eng@gmail.com>
2026-02-08 03:58:24 +0100

    Initial commit: base Spring Boot project with dependencies

---
+++ src/test/java/tn/finix/finixbackend/FinixBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.finix.finixbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinixBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
